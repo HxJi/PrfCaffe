9822cd78106b46f8cee04dbf84aae92cf7f9a333
hj <hj14@illinois.edu>
2019-03-20 19:55:05 -0500

    data structure

--- src/caffe/layers/pooling_layer.cu
+++ src/caffe/layers/pooling_layer.cu
@@ -243,12 +243,12 @@ void PoolingLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   }
 
   //[houxiang]
-  cudaMemcpy(zero_cell, dev_zero_cell, block_num * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&zero_cell, dev_zero_cell, block_num * sizeof(int), cudaMemcpyDeviceToHost);
   cudaFree(dev_zero_cell);
   int total_zero = 0;
   for(int i=0; i<block_num; ++i){
-	      total_zero = zero_cell[i];
-        sparsity_output << "zero_cell[" <<i<<"]:"<<zero_cell[i]<<std::endl;  
+	      total_zero = zero_cell[i] + total_zero;
+        sparsity_output << "zero_cell[" <<i<<"]:"<< zero_cell[i]<<std::endl;  
   }
   sparsity_output <<"total_zero:" << total_zero << std::endl;
 
