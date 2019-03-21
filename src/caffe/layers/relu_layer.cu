49f19d7c7c8670d1412e83f8dee57e25d65f17b7
hj <hj14@illinois.edu>
2019-03-20 23:04:08 -0500

    relu layer add

--- src/caffe/layers/relu_layer.cu
+++ src/caffe/layers/relu_layer.cu
@@ -7,9 +7,10 @@ namespace caffe {
 
 template <typename Dtype>
 __global__ void ReLUForward(const int n, const Dtype* in, Dtype* out,
-    Dtype negative_slope) {
+    Dtype negative_slope, int* zero_element) {
   CUDA_KERNEL_LOOP(index, n) {
     out[index] = in[index] > 0 ? in[index] : in[index] * negative_slope;
+    if(out[index] == 0) zero_element[blockIdx.x] += 1;
   }
 }
 
@@ -20,9 +21,40 @@ void ReLULayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   Dtype* top_data = top[0]->mutable_gpu_data();
   const int count = bottom[0]->count();
   Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
+
+  //[houxiang]
+  std::string filename = ("/home/hj14/caffe/hj_test/relu_sparsity.txt");
+  std::ofstream sparsity_output;
+  sparsity_output.open(filename.c_str(), ios::app);
+  //count the zero number in each block to save space
+  sparsity_output <<"topdata->count:" << count << std::endl;
+  int block_num = CAFFE_GET_BLOCKS(count);
+  int zero_cell[block_num];
+  for(int i=0; i<block_num; ++i){
+	  zero_cell[i] = 0;
+  }
+  cudaError_t err = cudaSuccess;
+  int *dev_zero_cell;
+  err = cudaMalloc((void**)&dev_zero_cell, block_num * sizeof(int));
+  if(err!=cudaSuccess) {
+        printf("the cudaMalloc on GPU is failed");
+   }
+  cudaMemcpy(dev_zero_cell, zero_cell, block_num * sizeof(int), cudaMemcpyHostToDevice);
+
   // NOLINT_NEXT_LINE(whitespace/operators)
   ReLUForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
-      count, bottom_data, top_data, negative_slope);
+      count, bottom_data, top_data, negative_slope, dev_zero_cell );
+  
+  //[houxiang]
+  cudaMemcpy(&zero_cell, dev_zero_cell, block_num * sizeof(int), cudaMemcpyDeviceToHost);
+  cudaFree(dev_zero_cell);
+  int total_zero = 0;
+  for(int i=0; i<block_num; ++i){
+	      total_zero = zero_cell[i] + total_zero;
+        //sparsity_output << "zero_cell[" <<i<<"]:"<< zero_cell[i]<<std::endl;  
+  }
+  sparsity_output <<"total_zero:" << total_zero << std::endl;
+  
   CUDA_POST_KERNEL_CHECK;
   // << " count: " << count << " bottom_data: "
   //     << (unsigned long)bottom_data
