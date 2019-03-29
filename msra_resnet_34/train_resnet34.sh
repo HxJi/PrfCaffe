39abf19b9803411f9982acacaabdd7aef748dcc5
houxiang <hj14@illinois.edu>
2019-03-29 14:42:10 -0500

    resnet description update

--- msra_resnet_34/train_resnet34.sh
+++ msra_resnet_34/train_resnet34.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-./build/tools/caffe train --solver=msra_resnet_34/resnet_34_solver.prototxt $@
+./build/tools/caffe train --solver=msra_resnet_34/resnet_34_solver.prototxt --snapshot=/home/hj14/snapshot/resnet_34_msra_iter_20000.solverstate $@
