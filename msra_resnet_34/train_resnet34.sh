f67d5befcc01655c68e285b29b86208e22a0cc87
houxiang <hj14@illinois.edu>
2019-03-31 23:22:00 -0500

    two run

--- msra_resnet_34/train_resnet34.sh
+++ msra_resnet_34/train_resnet34.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-./build/tools/caffe train --solver=msra_resnet_34/resnet_34_solver.prototxt --snapshot=/home/hj14/snapshot/resnet_34_msra_iter_60000.solverstate $@
+./build/tools/caffe train --solver=msra_resnet_34/resnet_34_solver.prototxt --snapshot=/home/hj14/caffe/resnet_34_msra_iter_80000.solverstate $@
